a6f5c53614c35a06f521c897a24932dee03ad455
hotdudehvfun <hotdudehvfun@gmail.com>
2017-06-21 10:14:22 +0530

    2

---
+++ set200/availlableTests.js
@@ -0,0 +1,8 @@
+[
+  {
+    "html": "<div class=\"test-card mdl-card mdl-shadow--2dp\">  <div class=\"test-card-title mdl-card__title mdl-card--expand\" >    <div class=\"test-card-title-text mdl-card__title-text\">ssc cpo mock test 1</div>  </div>  <div class=\"test-card-title-supporting-text mdl-card__supporting-text\">  <br>Time: 2 hours   <br>Total Questions:200  <br>Sample mock test  </div>  <div class=\"test-card-actions mdl-card__actions mdl-card--border\">    <a onclick=\"openTestArea('https://raw.githubusercontent.com/hotdudehvfun/allquestions/master/set200/set1.js')\" class=\"mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect\">Take Test</a>  </div></div>"
+  },
+  {
+    "html": "<div class=\"test-card mdl-card mdl-shadow--2dp\" >  <div class=\"test-card-title mdl-card__title mdl-card--expand\" >    <div class=\"test-card-title-text mdl-card__title-text\" >ssc cpo 2016 test</div>  </div>  <div class=\"test-card-title-supporting-text mdl-card__supporting-text\">    <br>Time: 2 hours   <br>Total Questions:200  <br>CPO previous year question 2016 june  </div>  <div class=\"test-card-actions mdl-card__actions mdl-card--border\">    <a onclick=\"openTestArea('https://raw.githubusercontent.com/hotdudehvfun/allquestions/master/set200/set2.js')\" class=\"mdl-button mdl-button--colored mdl-js-button mdl-js-ripple-effect\">Take Test</a>  </div></div>"
+  }
+]
