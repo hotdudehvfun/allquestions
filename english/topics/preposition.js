955fdf839765c56ecb0ffd998430c48e62fcaa8a
hotdudehvfun <hotdudehvfun@gmail.com>
2022-10-13 22:23:37 +0530

    Create preposition.js

---
+++ english/topics/preposition.js
@@ -0,0 +1,125 @@
+var items=[    {
+        "qtext": `Despite ever-growing public abhorrence ${blank} this most antisocial crime, numbers of drivers caught over the limit are rising`,
+        "options": ["A. for", "B. to", "C. of", "D. in"],
+        "correct": 2,
+    },
+    {
+        "qtext": "GRACEFUL",
+        "options": ["A. Rough", "B. Expert", "C. Miserable", "D. Awkward"],
+        "correct": 3,
+    },
+    {
+        "qtext": "NADIR",
+        "options": ["A. Modernity", "B. Zenith", "C. Liberty", "D. Progress"],
+        "correct": 1,
+    },
+    {
+        "qtext": "EXTRAVAGANCE",
+        "options": ["A. Luxury", "B. Poverty", "C. Economical", "D. Cheapness"],
+        "correct": 2,
+    },
+    {
+        "qtext": "PERTINENT",
+        "options": ["A. Irrational", "B. Irregular", "C. Insistent", "D. Irrelevant"],
+        "correct": 3,
+    },
+    {
+        "qtext": "OBSCURE",
+        "options": ["A. Implicit", "B. Obnoxious", "C. Explicit", "D. Pedantic"],
+        "correct": 2,
+    },
+    {
+        "qtext": "URBANE",
+        "options": ["A. Illiterate", "B. Backward", "C. Discourteous", "D. Orthodox"],
+        "correct": 2,
+    },
+    {
+        "qtext": "VANITY",
+        "options": ["A. Pride", "B. Humility", "C. Conceit", "D. Ostentatious"],
+        "correct": 1,
+    },
+    {
+        "qtext": "RARELY",
+        "options": ["A. Hardly", "B. Definately", "C. Frequently", "D. Periodically"],
+        "correct": 2,
+    },
+    {
+        "qtext": "MALICIOUS",
+        "options": ["A. Kind", "B. Boastful", "C. Generous", "D. Indifferent"],
+        "correct": 0,
+    },
+    {
+        "qtext": "EPILOGUE",
+        "options": ["A. Dialogue", "B. Prelude", "C. Post script", "D. Epigram"],
+        "correct": 1,
+    },
+    {
+        "qtext": "CAPACIOUS",
+        "options": ["A. Limited", "B. Caring", "C. Foolish", "D. Changeable"],
+        "correct": 0,
+    },
+    {
+        "qtext": "CONDENSE",
+        "options": ["A. Expand", "B. Distribute", "C. Interpret", "D. Lengthen"],
+        "correct": 0,
+    },
+    {
+        "qtext": "ADAPTABLE",
+        "options": ["A. Adoptable", "B. Flexible", "C. Yielding", "D. Rigid"],
+        "correct": 3,
+    },
+    {
+        "qtext": "SACROSANCT",
+        "options": ["A. Irreligious", "B. Unethical", "C. Irreverent", "D. Unholy"],
+        "correct": 3,
+    },
+    {
+        "qtext": "INDISCREET",
+        "options": ["A. Reliable", "B. Honest", "C. Prudent", "D. Stupid"],
+        "correct": 2,
+    },
+    {
+        "qtext": "FAMILIAR",
+        "options": ["A. Unpleasant", "B. Dangerous", "C. Friendly", "D. Strange"],
+        "correct": 3,
+    },
+    {
+        "qtext": "TANGIBLE",
+        "options": ["A. Ethereal", "B. Concrete", "C. Actual", "D. Solid"],
+        "correct": 0,
+    },
+    {
+        "qtext": "LOVE",
+        "options": ["A. Villainy", "B. Hatred", "C. Compulsion", "D. Force"],
+        "correct": 1,
+    },
+    {
+        "qtext": "FAMOUS",
+        "options": ["A. Disgraced", "B. Unknown", "C. Evil", "D. Popular"],
+        "correct": 1,
+    },
+    {
+        "qtext": "ABSOLUTE",
+        "options": ["A. Deficient", "B. Faulty", "C. Limited", "D. Scarce"],
+        "correct": 2,
+    },
+    {
+        "qtext": "FRUGAL",
+        "options": ["A. Copious", "B. Extravagant", "C. Generous", "D. Ostentatious"],
+        "correct": 1,
+    },
+    {
+        "qtext": "INSIPID",
+        "options": ["A. Tasty", "B. Stupid", "C. Discreet", "D. Feast"],
+        "correct": 0,
+    },
+    {
+        "qtext": "ABLE",
+        "options": ["A. Disable", "B. Inable", "C. Unable", "D. Misable"],
+        "correct": 2,
+    },
+    {
+        "qtext": "HOSTILITY",
+        "options": ["A. Courtesy", "B. Hospitality", "C. Relationship", "D. Friendliness"],
+        "correct": 3,
+    }]
